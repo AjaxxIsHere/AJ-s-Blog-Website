459950b92e04d258a5335cb0cae0695e5b03fd0c
AjaxxIsHere <ajaz2468@gmail.com>
2023-05-29 19:36:30 +0530

    Error Handling & extracting fetch request

--- src/Home.js
+++ src/Home.js
@@ -1,43 +1,26 @@
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  // State variable to store an array of blogs
-  const [blogs, setBlogs] = useState(null);
-
-  // Function to handle deleting a blog
-  const handleDelete = (id) => {
-    // Filter out the blog with the provided id and create a new array
-    const newBlogs = blogs.filter(blog => blog.id !== id);
-    // Update the blogs state with the new array
-    setBlogs(newBlogs);
-  }
-
-  // useEffect hook with an empty dependency array
-  useEffect(() => {
-    console.log("use effect ran");
   
-    fetch('http://localhost:8000/Blogs')
-      .then(res => res.json())  // <-- Return the promise from res.json()
-      .then(data => {
-        console.log(data);
-        setBlogs(data);
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
-      });
-  }, []);
-  
-
+  const { data: blogs, isLoading, error} = useFetch('http://localhost:8000/Blogs');
+ 
   return (
     <div className="home">
       
+      {error && <div> { error } </div>}
+
+      {isLoading && <div> Loading... </div>}
+
       {/* Render the BlogList component with all blogs */}
-      {blogs && <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
       
       
     </div>
   );
 }
+
+//Tutorial 20!
 
 export default Home;
